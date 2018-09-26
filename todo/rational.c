19a1e47c82077308613381b3f3664b712dfb895e
0dwalker <31480998+0dwalker@users.noreply.github.com>
2018-09-26 13:38:58 -0500

    Add files via upload

--- todo/rational.c
+++ todo/rational.c
@@ -1,57 +1,36 @@
-/*create the strucuture for a rational number
-*  value: type float: the quotient of two ints
-*  numerator: type int: the top
-*  denominator: type int: the bottom
-*/
-
 struct RationalNum {
     int num;
     int dom;
     double value;
-    //fill this in
-}
-
-
-//function for adding rational numbers
+};
 struct RationalNum add(struct RationalNum a, struct RationalNum b){   
     int num = a.num*b.dom+b.num*a.dom;
     int dom = a.dom*b.dom;
     int value = num/dom;
-    struct RationalNum c={num,dom,value }
-}
-  //store values in temporary variables
-    //do math
-    //create a new struct
-    //return the new struct
-
-//function for subtracting rational numbers
+    struct RationalNum c={num,dom,value};
+return c;
+};
 struct RationalNum subtract(struct RationalNum a, struct RationalNum b){
      int num = a.num*b.dom-b.num*a.dom;
     int dom = a.dom*b.dom;
     int value = num/dom;
-    struct RationalNum c={num,dom,value }
-}
-}
-
-//function for multiplying rational numbers
+    struct RationalNum c={num,dom,value};
+return c;
+};
 struct RationalNum multiply(struct RationalNum a, struct RationalNum b ){
     int num = a.num*b.num;
     int dom = a.dom*b.dom;
     int value = num/dom;
-    struct RationalNum c={num,dom,value }
-}
-
-//function for dividing rational numbers
-//cannot divide by zero
+    struct RationalNum c={num,dom,value};
+return c;
+};
 struct RationalNum divide(struct RationalNum a, struct RationalNum b ){
     int num = a.num*b.dom;
     int dom = a.dom*b.num;
     int value = num/dom;
-    struct RationalNum c={num,dom,value }
-
-}
-
-//function for adding rational numbers
+    struct RationalNum c={num,dom,value};
+return c;
+};
 struct RationalNum reduce(struct RationalNum a){
     int num = a.num;
     int dom = a.dom;
@@ -61,14 +40,15 @@ struct RationalNum reduce(struct RationalNum a){
             dom =dom/b; 
         b--;
         }
-        struct RationalNum c={num,dom,value }
-return c;
-        
     }
-}
-
+     struct RationalNum c={num,dom,0};
+return c;    
+};
 int main(){
-    struct RationalNum d={6,3,2}
-    struct RationalNum e=reduce(d);
-    printf("%i",e.num);
-}
+    struct RationalNum a={1,2,0.50};
+    struct RationalNum b={1,3,0.3333333333333333333};
+    printf("%i/%i\n",add(a,b).num, add(a,b).dom);
+    printf("%i/%i\n",subtract(a,b).num, subtract(a,b).dom);
+    printf("%i/%i\n", multiply(a,b).num, multiply(a,b).dom);
+    printf("%i/%i\n", divide(a,b).num, divide(a,b).dom);
+};
